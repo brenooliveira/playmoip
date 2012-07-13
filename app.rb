8c52091d551de0c1c4677743f675ba85071b3e35
Breno Oliveira <breno.oliveira@moip.com.br>
2012-07-13 18:19:37 -0300

    arranca o koala

--- app.rb
+++ app.rb
@@ -1,16 +1,8 @@
 require "sinatra"
-require 'koala'
 
 enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
-
-before do
-  # HTTPS redirect
-  if settings.environment == :production && request.scheme != 'https'
-    redirect "https://#{request.env['HTTP_HOST']}"
-  end
-end
 
 get "/" do
   erb :index
