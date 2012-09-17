e872def7b7cee9324c6d7be910387c88dbecc8a5
Breno Oliveira <breno26@gmail.com>
2012-09-16 21:23:53 -0300

    refatora para utilizar a versáo 002 do rmoip

--- app.rb
+++ app.rb
@@ -11,29 +11,31 @@ end
 
 post "/cobrar" do
     puts "Params #{params}"
-    moip = Rmoip.login "01010101010101010101010101010101", "ABABABABABABABABABABABABABABABABABABABAB", :SANDBOX
     dados_cobranca = {
-                        :valor => params[:valor],
-                        :id_proprio => params[:id_proprio],
-                        :razao => params[:razao],
-                        :pagador => {
-                          :nome => "Luiz Inácio Lula da Silva",
+                        :value => "8.90",
+                        :unique_id => params["id_proprio"],
+                        :reason => "Um motivo qualquer",
+                        :payer => {
+                          :name => "Luiz Inácio Lula da Silva",
                           :email => "presidente@planalto.gov.br",
-                          :tel_cel => "(61)9999-9999",
-                          :apelido => "Lula",
-                          :identidade => "111.111.111-11",
-                          :logradouro => "Praça dos Três Poderes",
-                          :numero => "0",
-                          :complemento => "Palácio do Planalto",
-                          :bairro => "Zona Cívico-Administrativa",
-                          :cidade => "Brasília",
-                          :estado => "DF",
-                          :pais => "BRA",
-                          :cep => "70100-000",
-                          :tel_fixo => "(61)3211-1221"
+                          :payer_id => "um-id-qualquer",
+                          :mobile_phone => "(61)9999-9999",
+                          :nickname => "Lula",
+                          :identity => "111.111.111-11",
+                          :street => "Praça dos Três Poderes",
+                          :number => "0",
+                          :complement => "Palácio do Planalto",
+                          :neighborhood => "Zona Cívico-Administrativa",
+                          :city => "Brasília",
+                          :state => "DF",
+                          :country => "BRA",
+                          :zipcode => "70100-000",
+                          :phone => "(61)3211-1221"
                         }
                       }
-    moip.cobrar(dados_cobranca)
-    response = moip.enviar_cobranca
-    puts "Response #{response}"
+    @response = Rmoip.sandbox("01010101010101010101010101010101","ABABABABABABABABABABABABABABABABABABABAB")
+        .send(dados_cobranca)
+    puts "Token #{response.token}"
+
+    erb :response
 end
